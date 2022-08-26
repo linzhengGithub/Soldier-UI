8d343f6aa75ea06a8d619c9470945aea76f62f4c
linzheng <alidlg9045@126.com>
2022-08-26 23:45:22 +0800

    配置vite build

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,30 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 
+const rollupOptions = {
+
+  external: ["vue", "vue-router"],
+  output: {
+    globals: {
+      vue: "Vue",
+    },
+  },
+};
+
 export default defineConfig({
 
   plugins: [vue()],
+  // 添加库模式配置
+  build: {
+    rollupOptions,
+    minify:false,
+    lib: {
+      entry: "./src/entry.ts",
+      name: "SoldierUI",
+      fileName: "soldier-ui",
+      // 导出模块格式
+      formats: ["es", "umd","iife"],
+    },
+  },
 
 });
